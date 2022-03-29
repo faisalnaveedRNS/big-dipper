e3d0484cb731d56a28fd615fa4f5c105e34af070
Kamran Durrani <kamran.durrani@rnssol.com>
2022-03-30 01:47:21 +0500

    Fix: NFT image URL request changed to RPC

--- imports/ui/easel_buy/EaselBuyContainer.js
+++ imports/ui/easel_buy/EaselBuyContainer.js
@@ -1,77 +1,84 @@
-import { Meteor } from 'meteor/meteor';
-import { withTracker } from 'meteor/react-meteor-data';
-import { Chain } from '/imports/api/chain/chain.js';
-import { Recipes } from '/imports/api/recipes/recipes.js';
-import EaselBuy from './EaselBuy.jsx';
-import { string } from 'prop-types';
+import { Meteor } from "meteor/meteor";
+import { withTracker } from "meteor/react-meteor-data";
+import { Chain } from "/imports/api/chain/chain.js";
+import { Recipes } from "/imports/api/recipes/recipes.js";
+import EaselBuy from "./EaselBuy.jsx";
+import { string } from "prop-types";
 
 export default HomeContainer = withTracker((props) => {
-    let recipesHandle;
-    let validatorsHandle;
-    let loading = true;
-    var name = '';
-    var description = '';
-    var img = '';
-    var url = props.url;
-    var price = '0 Pylon';
-    var selectedRecipe = null;
-    recipe_id = props.recipe_id  
+  return EaselBuy;
+  let recipesHandle;
+  let validatorsHandle;
+  let loading = true;
+  var name = "";
+  var description = "";
+  var img = "";
+  var url = props.url;
+  var price = "0 Pylon";
+  var selectedRecipe = null;
+  recipe_id = props.recipe_id;
 
-    if (Meteor.isClient) {
-        recipesHandle = Meteor.subscribe('recipes.list', recipe_id);
-        loading = !recipesHandle.ready();
+  if (Meteor.isClient) {
+    recipesHandle = Meteor.subscribe("recipes.list", recipe_id);
+    loading = !recipesHandle.ready();
+  }
+
+  let status;
+
+  if (Meteor.isServer || !loading) {
+    selectedRecipe = Recipes.findOne({ ID: recipe_id });
+  } else {
+    selectedRecipe = Recipes.findOne({ ID: recipe_id });
+  }
+
+  if (selectedRecipe != null) {
+    name = selectedRecipe.name;
+    description = selectedRecipe.description;
+    // if (description.length > 15) {
+    //     description = description.substring(0, 12) + '...';
+    // }
+    const coinInputs = selectedRecipe.coinInputs;
+    if (coinInputs.length > 0) {
+      if (coinInputs[0].coins[0].denom == "USD") {
+        price =
+          Math.floor(coinInputs[0].coins[0].amount / 100) +
+          "." +
+          (coinInputs[0].coins[0].amount % 100) +
+          " " +
+          coinInputs[0].coins[0].denom;
+      } else {
+        price =
+          coinInputs[0].coins[0].amount + " " + coinInputs[0].coins[0].denom;
+      }
     }
-
-    let status; 
-
-    if (Meteor.isServer || !loading) { 
-        selectedRecipe = Recipes.findOne({ ID: recipe_id });
-    }
-    else{
-        selectedRecipe = Recipes.findOne({ ID: recipe_id });
-    }    
-
-    if (selectedRecipe != null) {
-        name = selectedRecipe.name
-        description = selectedRecipe.description;
-        // if (description.length > 15) {
-        //     description = description.substring(0, 12) + '...';
-        // }
-        const coinInputs = selectedRecipe.coinInputs;
-        if (coinInputs.length > 0) {
-            if(coinInputs[0].coins[0].denom == "USD"){
-                price = Math.floor(coinInputs[0].coins[0].amount / 100) + '.' + (coinInputs[0].coins[0].amount % 100) + ' ' + coinInputs[0].coins[0].denom;
+    const entries = selectedRecipe.entries;
+    if (entries != null) {
+      const itemoutputs = entries.itemOutputs;
+      if (itemoutputs.length > 0) {
+        let strings = itemoutputs[0].strings;
+        for (i = 0; i < strings.length; i++) {
+          try {
+            if (
+              (strings[i].key =
+                "NFT_URL" && strings[i].value.indexOf("http") >= 0)
+            ) {
+              img = strings[i].value;
+              break;
             }
-            else{
-                price = coinInputs[0].coins[0].amount + ' ' + coinInputs[0].coins[0].denom
-            }
+          } catch (e) {
+            console.log("strings[i].value", e);
+            break;
+          }
         }
-        const entries = selectedRecipe.entries;
-        if (entries != null) {
-            const itemoutputs = entries.itemOutputs; 
-            if (itemoutputs.length > 0) {
-                let strings = itemoutputs[0].strings
-                for (i = 0; i < strings.length; i++) {
-                    try { 
-                        if (strings[i].key = "NFT_URL" && strings[i].value.indexOf('http') >= 0) {
-                            img = strings[i].value; 
-                            break;
-                        } 
-                    } catch (e) {
-                        console.log('strings[i].value', e)
-                        break;
-                    }
-
-                }
-            } 
-        }
+      }
     }
- 
-    return {
-        name,
-        description,
-        price,
-        img: img,
-        url: url,
-    };
+  }
+
+  return {
+    name,
+    description,
+    price,
+    img: img,
+    url: url,
+  };
 })(EaselBuy);
