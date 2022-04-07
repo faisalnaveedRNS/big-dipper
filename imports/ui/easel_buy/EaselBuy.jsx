4b32a30827ded9064bcee9f60ea19e7b8247665d
Mr. KF <khurram.farooq@rnssol.com>
2022-04-07 11:03:17 +0500

    fix: updated apple store url

--- imports/ui/easel_buy/EaselBuy.jsx
+++ imports/ui/easel_buy/EaselBuy.jsx
@@ -131,7 +131,7 @@ export default class EaselBuy extends Component {
     if (success) {
       const isMacLike = /(Mac|iPhone|iPod|iPad)/i.test(navigator.platform);
       if (isMacLike) {
-        window.location = "https://apps.apple.com/us/app/facebook/id1598732789"
+        window.location = "https://apps.apple.com/us/app/pylons/id1598732789"
       } else {
         window.location = "https://play.google.com/store/apps/details?id=tech.pylons.wallet&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1";
       }
